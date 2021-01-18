33d0919ededc57189e6d8bfe8ed8053aeab6d715
Cornelia van der Schilden <72788749+CorneliavanderSchilden@users.noreply.github.com>
2021-01-18 11:08:04 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,11 @@
+var number = 1;
+
+while (number < 21) {
+    if (number % 2 == 0) {
+        console.log('this is even: ', number)
+        number++
+    } else {
+        console.log('this is uneven: ', number)
+        number++
+    }
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,7 @@
+var x = 1;
+
+while (x <= 10) {
+    console.log(x + " * 9 = ", x * 9);
+    x++
+}
+
